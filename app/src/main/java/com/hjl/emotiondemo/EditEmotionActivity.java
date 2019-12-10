f260cddb3e3365fa5c9ab6db35235f092f5f8bf2
huangjieliang <410651732@qq.com>
2019-12-10 14:21:50 +0800

    ces

--- app/src/main/java/com/hjl/emotiondemo/EditEmotionActivity.java
+++ app/src/main/java/com/hjl/emotiondemo/EditEmotionActivity.java
@@ -1,7 +1,8 @@
 package com.hjl.emotiondemo;
 
-import android.support.v7.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -13,7 +14,6 @@ import android.widget.TextView;
 import com.hjl.emotiondemo.utils.EmojiKeyboard;
 import com.hjl.emotiondemo.utils.PanelKeyboard;
 import com.hjl.emotionpicker.EmotionView;
-
 
 public class EditEmotionActivity extends AppCompatActivity {
 
